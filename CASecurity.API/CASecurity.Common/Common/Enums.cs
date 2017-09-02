a448fe57b900f50b9a9949a8878cf8e1d446a1c3
Tharopan <tharopan@inexisconsulting.com>
2017-09-02 12:48:04 +0530

    Refactored: Adding common layer

---
+++ CASecurity.API/CASecurity.Common/Common/Enums.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CASecurity.Common
+{
+    public static class Enums
+    {
+        public enum APIResponseStatus
+        {
+            Success,
+            Falied,
+            NotFound,
+            BadRequest,
+            NoMatchingData,
+            MandataryFieldsAreMissing,
+            ServerError
+        }
+        public enum CallFrom
+        {
+            Bank,
+            Client,
+            SDK,
+            None
+        }
+    }
+}
