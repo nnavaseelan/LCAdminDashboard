e448cc23cdede1c65b4a79eceb2870d175041c74
tharopan <tharopan@gmail.com>
2017-09-07 22:10:36 +0530

    Refactored

---
+++ CASecurity/CASecurity.Web/Scripts/Views/apps.js
@@ -0,0 +1,207 @@
+﻿$(document).ready(function () {
+    loadAppList();
+
+    $('#btnSave').click(function (e) {
+        e.preventDefault();
+
+        var formData = {
+            'BankId': $('#BankId').val(),
+            'Name': $('input[name=Name]').val(),
+            'BRC': $('input[name=BRC]').val(),
+            'Address': $('input[name=Address]').val(),
+            'Code': $('input[name=Code]').val(),
+            'RedirectTo': 'App'
+        };
+        console.log(formData);
+
+        $.ajax({
+            url: "../Admin/Merchant",
+            type: "post",
+            data: formData,
+            success: function (d) {
+
+                clearMerchantInputs();
+                location.reload();
+
+            }
+        });
+
+    });
+
+    //$('#btnAppSave').click(function (e) {
+    //    e.preventDefault;
+
+    //    var formData = {
+    //        'BankId': $('#BankId').val(),
+    //        'MerchantId': $('#MerchantId').val(),
+    //        //'BMCode': $('input[name=BMCode]').val(),
+    //        'Name': $('input[name=AppName]').val(),
+    //        'Package': $('input[name=AppPackage]').val(),
+    //        'Address': $('input[name=AppAddress]').val(),
+    //        'Code': $('input[name=AppCode]').val(),
+    //        'PlatForm': $('input[name=AppPlatForm]').val(),
+    //        'Production': $('input[name=AppProduction]').val(),
+    //        'SandBox': $('input[name=AppSandBox]').val(),
+    //        'JustPayCode': '-'
+    //    };
+
+    //    $.ajax({
+    //        url: "/Admin/App",
+    //        type: "post",
+    //        data: formData,
+    //        success: function (d) {
+    //            loadAppList();
+    //            clearAppInputs();
+    //        }
+    //    });
+
+    //});
+
+    $('#btnAddMerchant').click(function () {
+        $.ajax({
+            type: "GET",
+            url: "/Admin/CreateMerchant",
+            contentType: "application/json; charset=utf-8",
+            data: { "Id": 0 },
+            datatype: "json",
+            success: function (data) {
+                debugger;
+                $("#createMerchant").html(data);
+                var selectedBank = $('#BankId option:selected').val();
+                $("#createMerchant").find("#BankId").val(selectedBank);
+                $("#createMerchant").find("#BankId").addClass("disabled");
+                $("#RedirectTo").val("App")
+                $('#createMerchantModel input:first-child').focus();
+                $('#createMerchantModel').modal();
+            },
+            error: function () {
+                console.log("Dynamic content load failed.");
+            }
+        });
+    });
+
+    $('#btnAddApp').click(function () {
+        //clearAppInputs();
+        //$('#myAppModal').modal();
+        debugger;
+        $.ajax({
+            type: "GET",
+            url: "/Admin/CreateApp",
+            contentType: "application/json; charset=utf-8",
+            data: { "Id": 0 },
+            datatype: "json",
+            success: function (data) {
+                debugger;
+                $("#createApp").html(data);
+                var selectedBank = $('#BankId option:selected').val();
+                $("#createAppModel").find("#BankId").val(selectedBank);
+
+                var selectedMerchant = $('#MerchantId option:selected').val();
+                $("#createAppModel").find("#MerchantId").val(selectedMerchant);
+
+
+                $('#createAppModel input:first-child').focus();
+                $('#createAppModel').modal();
+            },
+            error: function () {
+                console.log("Dynamic content load failed.");
+            }
+        });
+    });
+
+    $('.btnEdit').click(function () {
+        debugger;
+        var id = $(this).data("id");
+        $.ajax({
+            type: "GET",
+            url: "/Admin/CreateApp",
+            contentType: "application/json; charset=utf-8",
+            data: { "Id": id },
+            datatype: "json",
+            success: function (data) {
+                debugger;
+                $("#createApp").html(data);
+
+                var selectedBank = $('#BankId option:selected').val();
+                $("#createAppModel").find("#BankId").val(selectedBank);
+
+                var selectedMerchant = $('#MerchantId option:selected').val();
+                $("#createAppModel").find("#MerchantId").val(selectedMerchant);
+
+                $('#createAppModel input:first-child').focus();
+                $('#createAppModel').modal();
+            },
+            error: function () {
+                console.log("Dynamic content load failed.");
+            }
+        });
+    });
+
+    $('#MerchantId').change(function () {
+        loadAppList();
+    });
+
+    //$("#csrfile").fileupload({
+    //    autoUpload: true,
+    //    url: '/admin/UploadFile',
+    //    dataType: 'json',
+    //    add: function (e, data) {
+    //        var jqXHR = data.submit()
+    //            .success(function (data, textStatus, jqXHR) {
+    //                if (data.isUploaded) {
+
+    //                }
+    //                else {
+
+    //                }
+    //                alert(data.message);
+    //            })
+    //            .error(function (data, textStatus, errorThrown) {
+    //                if (typeof (data) != 'undefined' || typeof (textStatus) != 'undefined' || typeof (errorThrown) != 'undefined') {
+    //                    alert(textStatus + errorThrown + data);
+    //                }
+    //            });
+    //    },
+    //    fail: function (event, data) {
+    //        if (data.files[0].error) {
+    //            alert(data.files[0].error);
+    //        }
+    //    }
+    //});
+
+    function loadAppList() {
+        var bankId = $('#BankId').val();
+        var merchantId = $('#MerchantId').val();
+
+        url = "../Admin/AppList?bankId=" + bankId + "&merchantId=" + merchantId;
+
+        $.ajax({
+            url: url,
+            type: "get",
+            dataType: 'html',
+            contentType: 'application/html;'
+        }).
+            success(function (data) {
+                $('#divAppList').html(data);
+            })
+    }
+
+    function clearAppInputs() {
+        $('input[name=AppName]').focus();
+        $('input[name=AppName]').val("");
+        $('input[name=AppPackage]').val("");
+        $('input[name=AppAddress]').val("");
+        $('input[name=AppCode]').val("");
+        $('input[name=AppPlatForm]').val("");
+        $('input[name=AppProduction]').val("");
+        $('input[name=AppSandBox]').val("");
+    }
+
+    function clearMerchantInputs() {
+        $('input[name=Name]').focus();
+        $('input[name=Name]').val("");
+        $('input[name=BRC]').val("");
+        $('input[name=Address]').val("");
+        $('input[name=Code]').val("");
+    }
+});
---
+++ CASecurity/CASecurity.Web/Scripts/Views/bank.js
@@ -0,0 +1,87 @@
+﻿$(document).ready(function () {
+    //alert("loading");
+    $('#btnSave').click(function (e) {
+        e.preventDefault();
+        $('#createForm').submit();
+
+        //$("form[name='createForm']").validate({
+        //    // Specify validation rules
+        //    rules: {
+        //        // The key name on the left side is the name attribute
+        //        // of an input field. Validation rules are defined
+        //        // on the right side
+        //        name: "required",
+        //        address: "required",
+        //        contactPersonEmailId: {
+        //            required: true,
+        //            // Specify that email should be validated
+        //            // by the built-in "email" rule
+        //            email: true
+        //        },
+        //        code: {
+        //            required: true,
+        //            minlength:4
+        //        }
+        //    },
+        //    // Specify validation error messages
+        //    messages: {
+        //        name: "Please enter your firstname",
+        //        address: "Please enter your lastname",
+        //        code: {
+        //            required: "Please provide a password",
+        //            minlength: "Your password must be at least 5 characters long"
+        //        },
+        //        contactPersonEmailId: "Please enter a valid email address"
+        //    },
+        //    // Make sure the form is submitted to the destination defined
+        //    // in the "action" attribute of the form when valid
+        //    submitHandler: function (form) {
+        //        form.submit();
+        //    }
+        //});
+    });
+
+    $('#btnCreate').click(function () {
+        debugger;
+        $.ajax({
+            type: "GET",
+            url: "/Admin/CreateBank",
+            contentType: "application/json; charset=utf-8",
+            data: { "Id": 0 },
+            datatype: "json",
+            success: function (data) {
+                debugger;
+                $("#createBank").html(data);
+                $('#myModal input:first-child').focus();
+                $('#myModal').modal();
+
+            },
+            error: function () {
+                console.log("Dynamic content load failed.");
+            }
+        });
+    });
+
+    $('.btnEdit').click(function () {
+        debugger;
+        var id = $(this).data("id");
+        $.ajax({
+            type: "GET",
+            url: "/Admin/CreateBank",
+            contentType: "application/json; charset=utf-8",
+            data: { "Id": id },
+            datatype: "json",
+            success: function (data) {
+                debugger;
+                $("#createBank").html(data);
+                $('#myModal input:first-child').focus();
+                $('#myModal').modal();
+            },
+            error: function () {
+                console.log("Dynamic content load failed.");
+            }
+        });
+    });
+
+    
+});
